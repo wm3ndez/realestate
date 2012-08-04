bf51e127fdd877431c50f5cd0e90db7d79a6cf5f
Williams Mendez <wmendez27@gmail.com>
2012-08-04 17:07:35 -0400

    Templates and other fixes

---
+++ fabfile.py
@@ -0,0 +1,18 @@
+from fabric.api import local, run, env
+
+env.hosts = ['wmendez@108.59.4.79:22']
+
+def test_app(cli_args=''):
+    local('venv/bin/coverage -x holguinmatos/manage_local.py test %s' % cli_args)
+
+
+def run_server(cli_args=''):
+    local('venv/bin/python holguinmatos/manage_local.py runserver 0.0.0.0:8000 %s' % cli_args)
+
+
+def pushpull():
+    local('git push') # runs the command on the local environment
+    run('cd webapps/holguinmatos/; git pull; ./apache2/bin/restart') # runs the command on the remote environment
+
+def deploy():
+    run('cd webapps/holguinmatos/; git pull; ./apache2/bin/restart') # runs the command on the remote environment
