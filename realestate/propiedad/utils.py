0aaf4b6a10853d181cfa28037b3c50502698762c
Gustavo Rodriguez Baldera <gbaldera@gmail.com>
2014-04-06 20:06:30 -0400

    Adding attributes to realestate properties

---
+++ realestate/propiedad/utils.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+import string
+from decimal import Decimal
+
+
+def validation_simple(value, obj=None):
+    """
+    Validates that at least one character has been entered.
+    Not change is made to the value.
+    """
+    if len(value) >= 1:
+        return True, value, ''
+    else:
+        return False, value, u'El valor digitado debe tener uno o más caracteres'
+
+
+def validation_integer(value, obj=None):
+    """
+   Validates that value is an integer number.
+   No change is made to the value
+    """
+    try:
+        check = int(value)
+        return True, value, ''
+    except:
+        return False, value, u'El valor digitado no es un número entero'
+
+
+def validation_yesno(value, obj=None):
+    """
+    Validates that yes or no is entered.
+    Converts the yes or no to capitalized version
+    """
+    if string.upper(value) in ["SI", "NO"]:
+        return True, string.capitalize(value), ''
+    else:
+        return False, value, u'El valor digitado debe ser SI o NO'
+
+
+def validation_decimal(value, obj=None):
+    """
+    Validates that the number can be converted to a decimal
+    """
+    try:
+        check = Decimal(value)
+        return True, value, ''
+    except:
+        return False, value, u'El valor digitado debe ser un número decimal'
+
+
+def import_validator(validator):
+    try:
+        import_name, function_name = validator.rsplit('.', 1)
+    except ValueError:
+        # no dot; treat it as a global
+        func = globals().get(validator, None)
+        if not func:
+            # we use ImportError to keep error handling for callers simple
+            raise ImportError
+        return validator
+    else:
+        # The below __import__() call is from python docs, and is equivalent to:
+        #
+        #   from import_name import function_name
+        #
+        import_module = __import__(import_name, globals(), locals(), [function_name])
+
+        return getattr(import_module, function_name)
+
+
+def validate_attribute_value(attribute, value, obj):
+    """
+    Helper function for forms that wish to validation a value for an
+    AttributeOption.
+    """
+    return import_validator(attribute.validacion)(value, obj)
