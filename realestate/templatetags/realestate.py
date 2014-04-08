0d7b173b8ce0622c7f3f35a4d3584c07408c1119
Williams Mendez <wmendez27@gmail.com>
2014-04-08 18:29:37 -0400

    Fixed paths + removed newsletter code.

--- realestate/templatetags/realestate.py
+++ realestate/templatetags/realestate.py
@@ -1,6 +1,5 @@
 from django import template
-from propiedad.models import Propiedad
-from newsletter.models import Newsletter
+from realestate.propiedad.models import Propiedad
 
 
 register = template.Library()
@@ -21,24 +20,6 @@ def listado_features(propiedad):
 @register.inclusion_tag('home/carousel.html')
 def frontpage_carousel():
     propiedades = Propiedad.objects.activas().order_by('-id')[:4]
-#    import ipdb; ipdb.set_trace()
     return {
         'propiedades': propiedades
     }
-
-@register.inclusion_tag('forms/newsletter_header_form.html')
-def get_newsletter_form():
-    """
-     Newsletter Form
-    """
-    try:
-        newsletter = Newsletter.objects.get(slug='ultimas-noticias')
-        subscribe_url = newsletter.subscribe_url()
-    except Newsletter.DoesNotExist:
-        subscribe_url = '/'
-
-    return {
-        'subscribe_url':subscribe_url,
-
-    }
-
