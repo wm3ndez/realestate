f5ae6060487989b48c4735623ee03d80508995e2
Williams Mendez <wmendez27@gmail.com>
2014-04-24 11:11:41 -0400

    Filtering by last modification datetime

--- realestate/api/__init__.py
+++ realestate/api/__init__.py
@@ -29,3 +29,12 @@ class PropertySerializer(serializers.Serializer):
 class PropiedadViewSet(viewsets.ReadOnlyModelViewSet):
     model = Propiedad
     serializer_class = PropertySerializer
+
+    def get_queryset(self):
+        queryset = Propiedad.objects.all()
+
+        last_modified = self.request.QUERY_PARAMS.get('modified_from')
+        if last_modified is not None:
+            queryset = queryset.filter(ultima_modificacion__gt=last_modified)
+
+        return queryset
