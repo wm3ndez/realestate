317b55ad462fcbdba93f8c2dc9a817627d71bbb9
Williams Mendez <wmendez27@gmail.com>
2014-04-25 18:16:16 -0400

    Admin site: display listings(incomplete)

--- realestate/admin/urls.py
+++ realestate/admin/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import patterns, url
-from realestate.admin.views import Dashboard
+from realestate.admin.views import Dashboard, CreateListing, Listings
 
 urlpatterns = patterns(
     'realestate.admin.views',
     url('^$', Dashboard.as_view(), name='dashboard'),
+
+    url('^new-listing/', CreateListing.as_view(), name='add-listing'),
+    url('^listing/', Listings.as_view(), name='admin-list-listing'),
 
 )
--- realestate/admin/views.py
+++ realestate/admin/views.py
@@ -1,6 +1,16 @@
 from braces.views import StaffuserRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView, ListView
+from realestate.listing.models import Listing
 
 
 class Dashboard(TemplateView, StaffuserRequiredMixin):
     template_name = 'admin-panel/dashboard.html'
+
+
+class CreateListing(CreateView, StaffuserRequiredMixin):
+    template_name = 'admin-panel/create-listing.html'
+    model = Listing
+
+class Listings(ListView, StaffuserRequiredMixin):
+    template_name = 'admin-panel/listings.html'
+    model = Listing
