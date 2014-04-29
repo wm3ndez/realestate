bb8b92ffe2d28200a6d3ea1af8417ad03cfe9b73
Williams Mendez <wmendez27@gmail.com>
2014-04-28 23:33:04 -0400

    Bug fixes

--- fabfile.py
+++ fabfile.py
@@ -2,7 +2,7 @@ from fabric.api import local
 
 
 def test_app(cli_args=''):
-    local('venv/bin/coverage -x testproject/manage.py test %s' % cli_args)
+    local('venv/bin/python testproject/manage.py test %s' % cli_args)
 
 
 def runserver(cli_args=''):
