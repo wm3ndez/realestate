47343ecd0b631cdfec28ef26504f5bc396e76b8d
Williams Mendez <wmendez27@gmail.com>
2014-05-02 14:57:04 -0400

    updated dashboad template directory

--- realestate/admin/views.py
+++ realestate/admin/views.py
@@ -1,19 +1,19 @@
-from braces.views import StaffuserRequiredMixin
+from braces.views import StaffuserRequiredMixin, LoginRequiredMixin
 from django.views.generic import TemplateView, CreateView, ListView
-from realestate.listing.models import Listing
+from realestate.listing.models import Listing, Agent
 from realestate.admin.forms import ListingForm
 
 
 class Dashboard(TemplateView, StaffuserRequiredMixin):
-    template_name = 'admin-panel/dashboard.html'
+    template_name = 'dashboard/dashboard.html'
 
 
 class CreateListing(CreateView, StaffuserRequiredMixin):
-    template_name = 'admin-panel/create-listing.html'
+    template_name = 'dashboard/create-listing.html'
     model = Listing
     form_class = ListingForm
 
 
 class Listings(ListView, StaffuserRequiredMixin):
-    template_name = 'admin-panel/listings.html'
+    template_name = 'dashboard/listings.html'
     model = Listing
