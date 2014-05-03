e8f8c004719d30a706a8aaa4b9b80b97d9c0be86
Gustavo Rodriguez Baldera <gbaldera@gmail.com>
2014-05-03 15:29:46 -0400

    Adding the inline formsets for listing images and listing attributes

--- realestate/admin/forms.py
+++ realestate/admin/forms.py
@@ -1,8 +1,11 @@
-from django import forms
 from django.forms import ModelForm
-from realestate.listing.models import Listing, Agent
+from realestate.listing.models import Listing, AttributeListing, ListingImage
+from django.forms.models import inlineformset_factory
 
 
 class ListingForm(ModelForm):
     class Meta:
         model = Listing
+
+ListingImageFormSet = inlineformset_factory(Listing, ListingImage)
+AttributeListingFormSet = inlineformset_factory(Listing, AttributeListing)
--- realestate/admin/views.py
+++ realestate/admin/views.py
@@ -1,9 +1,10 @@
 from braces.views import StaffuserRequiredMixin, LoginRequiredMixin, OrderableListMixin
 from django.core.urlresolvers import reverse_lazy
 from django.views.generic import TemplateView, CreateView, ListView
+from django.http import HttpResponseRedirect
 from realestate.home.models import Contact
 from realestate.listing.models import Listing, Agent
-from realestate.admin.forms import ListingForm
+from realestate.admin.forms import ListingForm, ListingImageFormSet, AttributeListingFormSet
 
 
 class Dashboard(TemplateView, StaffuserRequiredMixin):
@@ -14,6 +15,34 @@ class CreateListing(CreateView, StaffuserRequiredMixin):
     template_name = 'dashboard/create-listing.html'
     model = Listing
     form_class = ListingForm
+
+    def get_context_data(self, **kwargs):
+        context = super(CreateListing, self).get_context_data(**kwargs)
+        if self.request.POST:
+            context['listing_images_form'] = ListingImageFormSet(self.request.POST, self.request.FILES)
+            context['listing_attributes_form'] = AttributeListingFormSet(self.request.POST, self.request.FILES)
+        else:
+            context['listing_images_form'] = ListingImageFormSet()
+            context['listing_attributes_form'] = AttributeListingFormSet()
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        listing_images_form = context['listing_images_form']
+        listing_attributes_form = context['listing_attributes_form']
+
+        if listing_images_form.is_valid() and form.is_valid() and listing_attributes_form.is_valid():
+            self.object = form.save()  # saves Father and Children
+            listing_images_form.instance = self.object
+            listing_images_form.instance.save()
+            listing_attributes_form.instance = self.object
+            listing_attributes_form.save()
+            return HttpResponseRedirect(self.get_success_url())
+        else:
+            return self.form_invalid(form)
+
+    def form_invalid(self, form):
+        return self.render_to_response(self.get_context_data(form=form))
 
 
 class Listings(ListView, StaffuserRequiredMixin):
