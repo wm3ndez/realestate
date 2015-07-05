453784acac31f37fba2304c7ada47cc8e2fa50ca
Williams Mendez <wmendez27@gmail.com>
2015-07-05 12:27:13 -0400

    Bumped version to 1.5.1

--- realestate/__init__.py
+++ realestate/__init__.py
@@ -3,7 +3,7 @@ __copyright__ = 'Copyright 2014, Williams Mendez'
 __credits__ = ['Williams Mendez, williamsmendez.com, @wmendezt']
 
 __license__ = 'BSD'
-__version__ = '1.5.0'
+__version__ = '1.5.1'
 __maintainer__ = 'Williams Mendez'
 __email__ = 'wmendez27@gmail.com'
 __status__ = 'Production'
